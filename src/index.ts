61fa91215d10ff3def3750ef55c6bc2bdc8f5ee6
phibar <philippbartels@outlook.com>
2022-02-01 09:47:45 +0100

    Added cli

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import * as oclif from "@oclif/core";
+import { finallyHandler } from "@typhonjs-oclif/core";
+
+/**
+ * Invokes the `fvttdev` CLI with args programmatically. Deletes any environment variables loaded from before to after
+ * execution.
+ *
+ * @param {...string} args - args to pass to CLI.
+ *
+ * @returns {Promise<void>}
+ */
+export default async function once(...args:any[]) {
+  return oclif.run(args, import.meta.url).finally(finallyHandler);
+}
