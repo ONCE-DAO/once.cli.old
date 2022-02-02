e943d68810ef062f8fba75d9f07e2650e41d24a3
phibar <philippbartels@outlook.com>
2022-02-02 19:17:05 +0100

    ...

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 import * as oclif from "@oclif/core";
 import { finallyHandler } from "@typhonjs-oclif/core";
-// import { BaseOnce } from "../../../../once.ts/dist/current/src/2_systems/Once/BaseOnce.class.js";
-
-// declare global {
-//   var ONCE: BaseOnce | undefined;
-// }
-
 /**
  * Invokes the `fvttdev` CLI with args programmatically. Deletes any environment variables loaded from before to after
  * execution.
