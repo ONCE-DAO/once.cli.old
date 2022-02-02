a04efd721d751d11d9bec87e16e5cd8fa8f98021
phibar <philippbartels@outlook.com>
2022-02-02 19:11:10 +0100

    quick fix

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import * as oclif from "@oclif/core";
 import { finallyHandler } from "@typhonjs-oclif/core";
-import { BaseOnce } from "../../../../once.ts/dist/current/src/2_systems/Once/BaseOnce.class.js";
+// import { BaseOnce } from "../../../../once.ts/dist/current/src/2_systems/Once/BaseOnce.class.js";
 
-declare global {
-  var ONCE: BaseOnce | undefined;
-}
+// declare global {
+//   var ONCE: BaseOnce | undefined;
+// }
 
 /**
  * Invokes the `fvttdev` CLI with args programmatically. Deletes any environment variables loaded from before to after
